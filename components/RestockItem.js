ccf87b4e603958addc942b12b354964432f22950
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-11-17 08:24:39 +0700

    consume index api

--- components/RestockItem.js
+++ components/RestockItem.js
@@ -1,20 +1,30 @@
 import styles from '../styles/Home.module.css'
 
-const RestockItem = ({ filled }) => {
+const formatDate = (date) => {
+  const d = new Date(date)
+  const year = d.getFullYear()
+  const month = d.toLocaleString('default', { month: 'long' })
+  const day = d.getDate()
+  return `${day} ${month} ${year}`
+}
+
+const RestockItem = ({ filled, restock }) => {
+  // console.log(restock)
   const conditionalStyles = `${styles.listItem} ${filled ? styles.bgBlue : ''}`
+  const date = formatDate(restock.date)
 
   return (
     <div className={conditionalStyles}>
       <div className={styles.grid}>
-        <h2 className={styles.flex}>17 November 2022 (80)</h2>
+        <h2 className={styles.flex}>{ date } ({ restock.quantity })</h2>
       </div>
       <div className={styles.grid}>
         <div className={styles.flex2}>
-          <p>Jumlah tabung masuk : 87</p>
-          <p>Jumlah tabung keluar : 36</p>
+          <p>Jumlah tabung masuk : {restock.totalDeliver}</p>
+          <p>Jumlah tabung keluar : {restock.totalPickup}</p>
         </div>
         <div className={`${styles.flex} ${styles.alignRight}`}>
-          <h1>44 | 43</h1>
+          <h1>{restock.restFilled} | {restock.restEmpty}</h1>
         </div>
       </div>
     </div>
